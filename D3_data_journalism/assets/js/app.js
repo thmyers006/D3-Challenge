84eaf87a6fa5e2d619d5697688b67f11c118e207
Theresa Myers <70074347+thmyers006@users.noreply.github.com>
2021-01-31 17:38:03 -0500

    2nd commit

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -5,7 +5,7 @@ var svgHeight = 600;
 var margin = {
   top: 20,
   right: 40,
-  bottom: 60,
+  bottom: 100,
   left: 100
 };
 
@@ -68,33 +68,41 @@ d3.csv("assets//js//data.csv").then(function(stateData) {
     .attr("cy", s => yLinearScale(s.age))
     .attr("r", "15")
     .attr("fill", "blue")
-    .attr("opacity", ".5")
-    .html(function(s) {
-        return(`${s.abbr}`);
-    });
-
+    .attr("opacity", ".5");
+    
+    var textGroup = chartGroup.selectAll("rect")
+      .data(stateData)
+      .enter()
+      .append("text")
+      .attr("x", s => xLinearScale(s.smokes) - 10)
+      .attr("y", s => yLinearScale(s.age) +5)
+      .style("fill", "black")
+      .text(function(s) {
+          return(`${s.abbr}`);
+      
+      });
     // Step 6: Initialize tool tip
     // ==============================
-    /* var toolTip = d3.tip()
+     var toolTip = d3.tip()
       .attr("class", "tooltip")
       .offset([80, -60])
-      .html(function(d) {
-        return (`${d.rockband}<br>Hair length: ${d.hair_length}<br>Hits: ${d.num_hits}`);
-      }); */
+      .html(function(s) {
+            return (`${s.state}<br> % of Smokers: ${s.smokes}<br>Average Age: ${s.age}`);
+      });
 
     // Step 7: Create tooltip in the chart
     // ==============================
-    //chartGroup.call(toolTip);
+    chartGroup.call(toolTip);
 
     // Step 8: Create event listeners to display and hide the tooltip
     // ==============================
-    //circlesGroup.on("click", function(data) {
-      //toolTip.show(data, this);
-    //})
+    circlesGroup.on("click", function(stateData) {
+      toolTip.show(stateData, this);
+    })
       // onmouseout event
-      //.on("mouseout", function(data, index) {
-        //toolTip.hide(data);
-      //});
+      .on("mouseout", function(stateData) {
+        toolTip.hide(stateData);
+    });
 
     // Create axes labels
     chartGroup.append("text")
